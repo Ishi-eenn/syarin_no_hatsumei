454b4668c8ca95d7b534b290e1df6c64d77f0c2f
kinakomoch7 <book-insect14@outlook.jp>
2023-10-20 20:43:44 +0900

    [add]コンポーネントわけ

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Top } from './src/components/pages/Top';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Top />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/pages/Top.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+
+
+export const Top = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
