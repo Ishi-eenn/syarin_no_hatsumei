7def8607e9ad61fa0f1ced98b361d5df4262de10
kinakomoch7 <book-insect14@outlook.jp>
2023-12-22 21:16:28 +0900

    [add]タブ変更と本棚情報を追加実装

--- src/components/templates/BookShelf.jsx
+++ src/components/templates/BookShelf.jsx
@@ -1,32 +1,34 @@
-import { Button, Card } from "antd-mobile"
-import { ShelfTier } from "../parts/ShelfTier"
+import React from "react";
+import { Button, Card } from "antd-mobile";
+import { ShelfTier } from "../parts/ShelfTier";
 
-export const BookShelf = () => {
+export const BookShelf = (props) => {
+  const { shelfData } = props;
 
-  const tier = ['a', 'b', 'c'];
+  const renderShelfTiers = (value) => {
+    console.log(shelfData)
+    const shelfTiers = [];
+    for (let i = 0; i < value; i++) {
+      shelfTiers.push(<ShelfTier key={i} />);
+    }
+    return shelfTiers;
+  };
 
   return (
-    <div style={{ width:'100%', height:'65vh', display:"flex" }}>
+    <div style={{ width: "100%", height: "65vh", display: "flex" }}>
       <div
         style={{
-          width:'80%',
-          height:'50vh',
-          margin:'auto',
-          justifyContent:"center",
+          width: "80%",
+          height: "50vh",
+          margin: "auto",
+          justifyContent: "center",
           borderWidth: 3,
           borderColor: "brown",
-          borderStyle: "solid"
-        }}>
-
-        <ShelfTier />
-        <ShelfTier />
-        <ShelfTier />
-
-        {tier.map( (item) => {
-          <ShelfTier />
-        })}
-
+          borderStyle: "solid",
+        }}
+      >
+        {renderShelfTiers(shelfData?.boards)}
       </div>
     </div>
-  )
-}
+  );
+};
