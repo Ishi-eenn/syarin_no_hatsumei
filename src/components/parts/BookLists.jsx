126f916f1d421ddc9d2e7c6d555008ed5965faa7
kinakomoch7 <book-insect14@outlook.jp>
2023-12-24 15:13:40 +0900

    [add]dbの初期化・全件取得の実装

--- src/components/parts/BookLists.jsx
+++ src/components/parts/BookLists.jsx
@@ -1,11 +1,12 @@
-import { InfiniteScroll, List, SwipeAction } from 'antd-mobile'
 import { useState } from 'react'
+import { FloatingBubble, InfiniteScroll, List, Modal, SwipeAction } from 'antd-mobile'
+import { AddCircleOutline } from 'antd-mobile-icons';
 
 export const BookLists = () => {
 
-    const [books, setBooks] = 
+    const [books, setBooks] =
         useState(['進撃の巨人', 'ワンピース', '情報可視化入門',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
         'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U'
        ])
 
@@ -15,6 +16,17 @@ export const BookLists = () => {
         const newBooks = [...books]
         newBooks.splice(index, 1)
         setBooks(newBooks)
+    }
+
+    const clickHandler = () => {
+        Modal.show({
+            content: '本の追加実装',
+            closeOnAction: true,
+            actions: [{
+                key:'add',
+                text: 'add',
+            }]
+        })
     }
 
     return (
@@ -31,6 +43,14 @@ export const BookLists = () => {
                 ))}
             </List>
             <InfiniteScroll />
+            <FloatingBubble
+                style={{
+                    '--initial-position-bottom': '100px',
+                    '--initial-position-right': '30px',
+                    '--edge-distance': '50px',}}
+                onClick={clickHandler} >
+                    <AddCircleOutline fontSize={50} />
+                </FloatingBubble>
         </>
     )
 }
