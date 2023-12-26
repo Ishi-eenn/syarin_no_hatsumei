1a2549faa85b9b131131aed5a2fa26a2dfc4e299
Kawabe Kaito <chka21055@g.nihon-u.ac.jp>
2023-12-26 23:57:13 +0900

    Tab幅揃えた

--- src/components/pages/AddPage.jsx
+++ src/components/pages/AddPage.jsx
@@ -20,34 +20,35 @@ export const AddPage = () => {
 
   return (
     <>
-      <AutoCenter>
-        <Tabs>
-          <Tabs.Tab title={tabItems[0].title} key={tabItems[0].key}>
-            <Card
-              title="本の追加"
-              bodyStyle={{ border: "solid 2px", background: "gray" }}
-            >
-              <Radio.Group>
-                <Radio value="all">完全一致</Radio>
-                <Radio value="series">シリーズ検索</Radio>
-              </Radio.Group>
+      <Tabs>
+        <Tabs.Tab
+          title={tabItems[0].title}
+          key={tabItems[0].key}
+        >
+          <Card
+            title="本の追加"
+            bodyStyle={{ border: "solid 2px", background: "gray" }}
+          >
+            <Radio.Group>
+              <Radio value="all">完全一致</Radio>
+              <Radio value="series">シリーズ検索</Radio>
+            </Radio.Group>
 
-              <Form layout="vertical" mode="card">
-                <Form.Item extra={<a>検索</a>}>
-                  <Input placeholder="" />
-                </Form.Item>
-              </Form>
-            </Card>
-          </Tabs.Tab>
-          <Tabs.Tab title={tabItems[1].title} key={tabItems[1].key}></Tabs.Tab>
-        </Tabs>
-        <AutoCenter>
-          <NormalButton
-            text="追加"
-            style={{ color: "primary", fill: "solid" }}
-            onClick={changeHandler}
-          />
-        </AutoCenter>
+            <Form layout="vertical" mode="card">
+              <Form.Item extra={<a>検索</a>}>
+                <Input placeholder="" />
+              </Form.Item>
+            </Form>
+          </Card>
+        </Tabs.Tab>
+        <Tabs.Tab title={tabItems[1].title} key={tabItems[1].key}></Tabs.Tab>
+      </Tabs>
+      <AutoCenter>
+        <NormalButton
+          text="追加"
+          style={{ color: "primary", fill: "solid" }}
+          onClick={changeHandler}
+        />
       </AutoCenter>
     </>
   );
