7ad28568503d81caacf2ec682f36ae3925c2a72d
inoue_r <chru21072@g.nihon-u.ac.jp>
2023-12-27 01:38:16 +0900

    テストデータを作成

--- src/features/test/TestData.jsx
+++ src/features/test/TestData.jsx
@@ -1,42 +1,67 @@
+/* 単位はmmで統一 */
+
 export const TEST_DATA = [
   {
-    id: 1,
-    name: user
-  }
-]
-
-export const TEST_DATA_BOOKS = [
-  {
-    isbn: 9784065334928,
-    bookName: '中間管理録トネガワ',
-    pageCount: 208,
-    id: 1,
+    title: "ストック",
+    books: [
+      {
+        isbn: 9784065334928,
+        bookName: "中間管理録トネガワ",
+        pageCount: 208,
+        id: 1,
+      },
+      {
+        isbn: 9784065327722,
+        bookName: "星降る王国のニナ（11）",
+        pageCount: 176,
+        id: 1,
+      },
+    ],
   },
   {
-    isbn: 9784065327722,
-    bookName: '星降る王国のニナ（11）',
-    pageCount: 176,
-    id: 1,
-  }
-]
-
-export const TEST_DATA_SHELVE = [
-  {
-    title: '本棚1',
-    id: '1',
-    unit: '',
-    h: '120',
-    w: '50',
-    boards: '2',
-    roomH: '40'
+    title: "本棚1",
+    id: "1",
+    h: "120",
+    w: "50",
+    /* 段数分の空配列追加して作成 例:2段分のため2つの空配列を用意 */
+    books: [[], []],
   },
   {
-    title: '本棚2',
-    id: '2',
-    unit: '',
-    h: '120',
-    w: '70',
-    boards: '3',
-    roomH: '30'
-  }
-]
+    title: "本棚2",
+    id: "2",
+    h: "120",
+    w: "70",
+    books: [
+      [
+        {
+          isbn: 9784065334928,
+          bookName: "中間管理録トネガワ",
+          pageCount: 208,
+          id: 1,
+        },
+        {
+          isbn: 9784065327722,
+          bookName: "星降る王国のニナ（11）",
+          pageCount: 176,
+          id: 1,
+        },
+      ],
+      [
+        {
+          isbn: 9784065334928,
+          bookName: "情報可視化入門",
+          pageCount: 208,
+          id: 1,
+        },
+      ],
+    ],
+  },
+];
+
+/* 初期状態 */
+export const DEFAULT_DATA = [
+  {
+    title: "ストック",
+    books: [],
+  },
+];
