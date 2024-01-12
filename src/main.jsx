c6a545d848905e6703060b70f912244043d37307
Ishi <syi378039@gmail.com>
2024-01-12 15:39:29 +0900

    createContextでmainにstateを作って他のcomponentからuseContextでよ別用にしてます

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Top } from './components/pages/Top'
 import { NotFound } from './components/pages/NotFound'
-import React from 'react'
+import React, { createContext } from 'react'
 import { FooterTabs } from './common/Footer/FooterTabs'
 import { AddPage } from './components/pages/AddPage'
 import { ListPage } from './components/pages/ListPage'
 import { SafeArea } from 'antd-mobile'
 
+export const BookDataContext = createContext();
+
 export const Main = () => {
+  const [bookData, setBookData] = React.useState([]);
   return (
     <div style={{ height:'100vh' }}>
-      <BrowserRouter>
-        <SafeArea position='top' style={{ height:'5vh' }} />
-        <Routes >
-          <Route exact path='/' element={ <Top /> }/>
-          {/* <Route path='/add' element={ <AddPage /> } /> */}
-          <Route path='/list' element={ <ListPage /> } />
-          <Route path='*' element={ <NotFound /> } />
-        </Routes>
-        <FooterTabs />
-      </BrowserRouter>
+      <BookDataContext.Provider value={[bookData, setBookData]}>
+        <BrowserRouter>
+          <SafeArea position='top' style={{ height:'5vh' }} />
+          <Routes >
+            <Route exact path='/' element={ <Top /> }/>
+            <Route path='/list' element={ <ListPage /> } />
+            <Route path='*' element={ <NotFound /> } />
+          </Routes>
+          <FooterTabs />
+        </BrowserRouter>
+      </BookDataContext.Provider>
     </div>
   )
 }
